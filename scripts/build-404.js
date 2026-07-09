3e50528461eb2eb82019dee826a26a0fe1b8b520
Jeff K <jeffery.keene@gsa.gov>
2026-07-09 16:23:58 -0400

    fixed postscript 404.html build

--- scripts/build-404.js
+++ scripts/build-404.js
@@ -8,14 +8,14 @@ console.log('Fixing 404.html paths...');
 
 try {
 	let content = readFileSync(filePath, 'utf-8');
-	
+
 	// Replace absolute paths with relative paths in attributes
 	content = content.replace(/href="\//g, 'href="./');
 	content = content.replace(/src="\//g, 'src="./');
-	
+
 	// Replace absolute paths in JavaScript import() statements
 	content = content.replace(/import\("\//g, 'import("./');
-	
+
 	writeFileSync(filePath, content);
 	console.log(`✓ Fixed paths in ${filePath}`);
 } catch (error) {

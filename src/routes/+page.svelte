4b7d3b21da83065e9c66018ec1c2b704dcac09cc
Jeff K <jeffery.keene@gsa.gov>
2026-07-14 15:44:16 -0400

    Added the Ready Section on the home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { Button } from '@gsa-tts/svelte-ui-uswds';
+	import { Button, Accordion, AccordionItem } from '@gsa-tts/svelte-ui-uswds';
 
 	function handleGetStarted() {
 		window.location.href = 'https://gsaext-fpdf-uat.cloud.databricks.com/';
@@ -24,9 +24,52 @@
 		</div>
 	</div>
 </section>
-
+<section id="test-section-id" class="grid-container usa-section  bg-primary-lighter ">
+	<div class="grid-row grid-gap">
+		<div class="tablet:grid-col-5">
+			<h2 class="font-heading-xl margin-y-0">Ready to get started?</h2>
+			<p>
+				FPDF is available exclusively to federal employees and authorized contractors. Contact our team to set up your agency’s access.
+			</p>
+			<Button variant="default" class="margin-top-4">Contact us</Button>
+		</div>
+		<div class="tablet:grid-col-1 usa-prose"></div>
+		<div class="tablet:grid-col-6 usa-prose">
+			<Accordion bordered class="custom-accordion">
+				<AccordionItem id="who" title="Who can access FPDF?" expanded>
+					FPDF is available to federal employees with a valid Login.gov account. Access levels may vary by agency and role. Contract your agency’s FPDF point of contact to learn more about access permissions.
+				</AccordionItem>
+				<AccordionItem id="what" title="What datasets are included in FPDF?">
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit
+				</AccordionItem>
+				<AccordionItem id="do" title="Do I need technical skills to use FPDF?">
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit
+				</AccordionItem>
+				<AccordionItem id="is" title="Is my agency’s data secure?">
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit
+				</AccordionItem>
+			</Accordion>
+		</div>
+	</div>
+</section>
 
 <style>
+	:global(.custom-accordion .usa-accordion__button) {
+		background-color:  #aacdec;
+	}
+	:global(.custom-accordion .usa-accordion__button:hover) {
+		background-color: #73b3e7;
+	}
+	:global(.custom-accordion .usa-accordion__button:focus) {
+		outline: 3px solid #aacdec;
+		outline-offset: 2px;
+	}
+	:global(.custom-accordion .usa-accordion__content) {
+		background-color: #d9e8f6;
+		border-left-color: #aacdec;
+		border-right-color: #aacdec;
+		border-bottom-color: #aacdec;
+	}
 	.usa-hero {
 		color: rgb(0, 0, 0);
 		background-color: rgb(240, 240, 240);

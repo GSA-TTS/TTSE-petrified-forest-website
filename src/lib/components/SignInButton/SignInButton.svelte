39c6d36e4e887228034a524666964f04c6bc96ad
Jeff K <jeffery.keene@gsa.gov>
2026-07-20 11:38:02 -0400

    fixed feedback for home page

---
+++ src/lib/components/SignInButton/SignInButton.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { Button } from '@gsa-tts/svelte-ui-uswds';
+
+	function handleGetStarted() {
+		window.location.href = 'https://gsaext-fpdf-uat.cloud.databricks.com/';
+	}
+</script>
+
+<Button variant="default" onclick={handleGetStarted}>Sign in</Button>
